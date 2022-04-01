0e83fa958ebbc31ea3e7d5d7645681c7b0c773fb
ezhang123 <76235043+ezhang123@users.noreply.github.com>
2022-04-01 18:06:14 -0400

    Added skeletons for sign in page
    The general basics of a login page, will add more after database is shared

--- SignIn.php
+++ SignIn.php
@@ -3,11 +3,40 @@
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Login into your account</title>
 </head>
 <body>
+
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
 <?php
+session_start();
+include "database_placeholder.php"; //replace this
+
+//Function for validating data, may not be needed
+if(isset($_POST['username']) && isset($_POST['password'])) {
+
+    function validate($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+}
+
+$username = validate($_POST['username']);
+$password = validate($_POST['password']);
+
+//Check for username and password entry
+if(empty($user)) {
+    header ("Location: index.php?error=Username is Required");
+    exit();
+}
+else if (empty($password)) {
+    header ("Location: index.php?error=Password is Required");
+    exit();
+}
+
+?>
