d88a154298dd15b7980854df3197e2213781b33a
vshah <vshah2799@gmail.com>
2022-04-03 11:38:50 -0400

    Comments

--- dbFiles/UsingAccountObjecTemplate.php
+++ dbFiles/UsingAccountObjecTemplate.php
@@ -2,21 +2,30 @@
 include("PushDataIntoDB.php");
 include("StudentOrRefugeeAccountObject.php");
 
+//Create a new Account Object. For the ROA or School, replace StudentOrRefugeeAccountObject with
+//the name of the respective class
 $testObject = new StudentOrRefugeeAccountObject();
-$testObject->setAge(2);
-$testObject->setSubjects(["Math", "English"]);
 
 //These are a requirement when creating the object. If at least 1 of these is not there
 //You must through and error. This check will be done on the FE
 $testObject->setUsername("vshah");
 $testObject->setPassword("1234");
 $testObject->setAccountType("Student");
+//////////////////////////////////////////////////
 
-//Don't wanna try and push the same object twice so this part is just to see
+//This is how you would set the other attributes.
+//Subjects and Hobbies/Interests are arrays
+$testObject->setAge(2);
+$testObject->setSubjects(["Math", "English"]);
+
+
+
+//This object is already in the DB so can't push it again
 //pushAccountObjectIntoDB($testObject->getUsername(), $testObject->getAccountType(), $testObject, "sql2.njit.edu", "vs598", "7p984^KTdv@M8o^");
 
 //Gets an account object with the specified Username and Account Type
 $testObjectTwo = getAccountObject("vshah", "Student", "sql2.njit.edu", "vs598", "7p984^KTdv@M8o^");
 
-//All field of the Object can now be accessed
+//All fields of the Object can now be accessed like this
+//For signing in, get the password from this and make sure they match for verification
 print($testObjectTwo->getUsername());
