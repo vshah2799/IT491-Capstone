d64f715398f62bb7566393b51908887be8fa7065
ezhang123 <76235043+ezhang123@users.noreply.github.com>
2022-04-04 20:38:10 -0400

    Skeleton for login is done, just need db

--- SignIn.php
+++ SignIn.php
@@ -39,4 +39,27 @@ else if (empty($password)) {
     exit();
 }
 
+$sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+
+$result = mysqli_query($conn, $sql); //watch out for $conn
+
+if(mysqli_num_rows($result) === 1) {
+    $row = mysqli_fetch_assoc($result);
+    if($row['username'] === $username && $row['password'] === $password) {
+        echo 'Successfully logged in!';
+        $_SESSION['username'] = $row['username'];
+        $_SESSION['name'] = $row['name'];
+        $_SESSION['id'] = $row['id'];
+        header("Location: home.php"); //placeholder for homepage
+        exit();
+    }
+    else{
+        header ("Location: index.php?error=Incorrect username or password");
+    }
+}
+else{
+    header("Location: index.php");
+    exit();
+}
+
 ?>
