6915a262a10f3156f138c21d97aa19cd7507d7d5
Karel Rojas <karelrojas64@gmail.com>
2022-04-17 23:06:08 -0400

    Added account edit pages and confirmation pages

---
+++ html/sign_up_-_refugee_org_created.php
@@ -0,0 +1,223 @@
+﻿<!DOCTYPE html>
+<html>
+  <head>
+    <title>Sign Up - Refugee Org Created</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="resources/css/axure_rp_page.css" type="text/css" rel="stylesheet"/>
+    <link href="data/styles.css" type="text/css" rel="stylesheet"/>
+    <link href="files/sign_up_-_refugee_org_created/styles.css" type="text/css" rel="stylesheet"/>
+    <script src="resources/scripts/jquery-3.2.1.min.js"></script>
+    <script src="resources/scripts/axure/axQuery.js"></script>
+    <script src="resources/scripts/axure/globals.js"></script>
+    <script src="resources/scripts/axutils.js"></script>
+    <script src="resources/scripts/axure/annotation.js"></script>
+    <script src="resources/scripts/axure/axQuery.std.js"></script>
+    <script src="resources/scripts/axure/doc.js"></script>
+    <script src="resources/scripts/messagecenter.js"></script>
+    <script src="resources/scripts/axure/events.js"></script>
+    <script src="resources/scripts/axure/recording.js"></script>
+    <script src="resources/scripts/axure/action.js"></script>
+    <script src="resources/scripts/axure/expr.js"></script>
+    <script src="resources/scripts/axure/geometry.js"></script>
+    <script src="resources/scripts/axure/flyout.js"></script>
+    <script src="resources/scripts/axure/model.js"></script>
+    <script src="resources/scripts/axure/repeater.js"></script>
+    <script src="resources/scripts/axure/sto.js"></script>
+    <script src="resources/scripts/axure/utils.temp.js"></script>
+    <script src="resources/scripts/axure/variables.js"></script>
+    <script src="resources/scripts/axure/drag.js"></script>
+    <script src="resources/scripts/axure/move.js"></script>
+    <script src="resources/scripts/axure/visibility.js"></script>
+    <script src="resources/scripts/axure/style.js"></script>
+    <script src="resources/scripts/axure/adaptive.js"></script>
+    <script src="resources/scripts/axure/tree.js"></script>
+    <script src="resources/scripts/axure/init.temp.js"></script>
+    <script src="resources/scripts/axure/legacy.js"></script>
+    <script src="resources/scripts/axure/viewer.js"></script>
+    <script src="resources/scripts/axure/math.js"></script>
+    <script src="resources/scripts/axure/jquery.nicescroll.min.js"></script>
+    <script src="data/document.js"></script>
+    <script src="files/sign_up_-_refugee_org_created/data.js"></script>
+    <script type="text/javascript">
+      $axure.utils.getTransparentGifPath = function() { return 'resources/images/transparent.gif'; };
+      $axure.utils.getOtherPath = function() { return 'resources/Other.html'; };
+      $axure.utils.getReloadPath = function() { return 'resources/reload.html'; };
+    </script>
+  </head>
+  <body>
+    <div id="base" class="">
+
+      <!-- Unnamed (Header) -->
+
+      <!-- Unnamed (Rectangle) -->
+      <div id="u97" class="ax_default heading_1">
+        <div id="u97_div" class=""></div>
+        <div id="u97_text" class="text ">
+          <p><span><a href="home.php">REDU</a></span></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Rectangle) -->
+      <div id="u98" class="ax_default link_button">
+        <div id="u98_div" class=""></div>
+        <div id="u98_text" class="text ">
+          <p><span><a href="sign_up_-_selection_between_refugee_and_student.php">SIGN UP</a></span></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Rectangle) -->
+      <div id="u99" class="ax_default link_button">
+        <div id="u99_div" class=""></div>
+        <div id="u99_text" class="text ">
+          <p><span><a href="about_us.php">ABOUT</a></span></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Rectangle) -->
+      <div id="u100" class="ax_default link_button">
+        <div id="u100_div" class=""></div>
+        <div id="u100_text" class="text ">
+          <p><span><a href="contact_us.php">CONTACT US</a></span></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Rectangle) -->
+      <div id="u101" class="ax_default link_button">
+        <div id="u101_div" class=""></div>
+        <div id="u101_text" class="text ">
+          <p><span><a href="sign_in.php">LOG IN</a></span></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Shape) -->
+      <div id="u102" class="ax_default icon">
+        <img id="u102_img" class="img " src="images/home/u27.svg"/>
+        <div id="u102_text" class="text " style="display:none; visibility: hidden">
+          <p></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Rectangle) -->
+      <div id="u103" class="ax_default link_button">
+        <div id="u103_div" class=""></div>
+        <div id="u103_text" class="text ">
+          <p><span>CONNECT</span></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Image) -->
+      <div id="u104" class="ax_default image">
+      <a href="home.php">
+        <img id="u104_img" class="img " src="images/home/u29.png"/>
+      </a>
+        <div id="u104_text" class="text " style="display:none; visibility: hidden">
+          <p></p>
+        </div>
+      </div>
+      <div id="u96" style="display:none; visibility:hidden;"></div>
+
+      <!-- Unnamed (Rectangle) -->
+      <div id="u105" class="ax_default">
+        <div id="u105_div" class=""></div>
+        <div id="u105_text" class="text " style="display:none; visibility: hidden">
+          <p></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Shape) -->
+      <div id="u106" class="ax_default icon">
+        <img id="u106_img" class="img " src="images/sign_up_-_student_created/u40.svg"/>
+        <div id="u106_text" class="text " style="display:none; visibility: hidden">
+          <p></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Shape) -->
+      <div id="u107" class="ax_default icon">
+        <img id="u107_img" class="img " src="images/sign_up_-_student_created/u41.svg"/>
+        <div id="u107_text" class="text " style="display:none; visibility: hidden">
+          <p></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Shape) -->
+      <div id="u108" class="ax_default icon">
+        <img id="u108_img" class="img " src="images/sign_up_-_student_created/u42.svg"/>
+        <div id="u108_text" class="text " style="display:none; visibility: hidden">
+          <p></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Shape) -->
+      <div id="u109" class="ax_default icon">
+        <img id="u109_img" class="img " src="images/sign_up_-_student_created/u43.svg"/>
+        <div id="u109_text" class="text " style="display:none; visibility: hidden">
+          <p></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Rectangle) -->
+      <div id="u110" class="ax_default heading_3">
+        <div id="u110_div" class=""></div>
+        <div id="u110_text" class="text ">
+          <p><span>© [Company Name] 2022, All Rights Reserved</span></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Rectangle) -->
+      <div id="u111" class="ax_default heading_3">
+        <div id="u111_div" class=""></div>
+        <div id="u111_text" class="text ">
+          <p><span>Terms Of Service</span></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Rectangle) -->
+      <div id="u112" class="ax_default heading_3">
+        <div id="u112_div" class=""></div>
+        <div id="u112_text" class="text ">
+          <p><span>Privacy Policy</span></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Rectangle) -->
+      <div id="u113" class="ax_default heading_3">
+        <div id="u113_div" class=""></div>
+        <div id="u113_text" class="text ">
+          <p><span>Volunteer</span></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Rectangle) -->
+      <div id="u114" class="ax_default box_1">
+        <div id="u114_div" class=""></div>
+        <div id="u114_text" class="text " style="display:none; visibility: hidden">
+          <p></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Rectangle) -->
+      <div id="u115" class="ax_default box_1">
+        <div id="u115_div" class=""></div>
+        <div id="u115_text" class="text " style="display:none; visibility: hidden">
+          <p></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Rectangle) -->
+      <div id="u116" class="ax_default heading_1">
+        <div id="u116_div" class=""></div>
+        <div id="u116_text" class="text ">
+          <p><span>Refugee Org Account Created</span></p>
+        </div>
+      </div>
+
+      <div class="mx-auto" style="width: 200px;">
+        <a href="sign_in.php" id="SignIn" type="button" class="btn btn-secondary" >Sign In</a>
+      </div>
+    </div>
+    <script src="resources/scripts/axure/ios.js"></script>
+  </body>
+</html>
---
+++ html/sign_up_-_school_created.php
@@ -0,0 +1,223 @@
+﻿<!DOCTYPE html>
+<html>
+  <head>
+    <title>Sign Up - School Created</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="resources/css/axure_rp_page.css" type="text/css" rel="stylesheet"/>
+    <link href="data/styles.css" type="text/css" rel="stylesheet"/>
+    <link href="files/sign_up_-_school_created/styles.css" type="text/css" rel="stylesheet"/>
+    <script src="resources/scripts/jquery-3.2.1.min.js"></script>
+    <script src="resources/scripts/axure/axQuery.js"></script>
+    <script src="resources/scripts/axure/globals.js"></script>
+    <script src="resources/scripts/axutils.js"></script>
+    <script src="resources/scripts/axure/annotation.js"></script>
+    <script src="resources/scripts/axure/axQuery.std.js"></script>
+    <script src="resources/scripts/axure/doc.js"></script>
+    <script src="resources/scripts/messagecenter.js"></script>
+    <script src="resources/scripts/axure/events.js"></script>
+    <script src="resources/scripts/axure/recording.js"></script>
+    <script src="resources/scripts/axure/action.js"></script>
+    <script src="resources/scripts/axure/expr.js"></script>
+    <script src="resources/scripts/axure/geometry.js"></script>
+    <script src="resources/scripts/axure/flyout.js"></script>
+    <script src="resources/scripts/axure/model.js"></script>
+    <script src="resources/scripts/axure/repeater.js"></script>
+    <script src="resources/scripts/axure/sto.js"></script>
+    <script src="resources/scripts/axure/utils.temp.js"></script>
+    <script src="resources/scripts/axure/variables.js"></script>
+    <script src="resources/scripts/axure/drag.js"></script>
+    <script src="resources/scripts/axure/move.js"></script>
+    <script src="resources/scripts/axure/visibility.js"></script>
+    <script src="resources/scripts/axure/style.js"></script>
+    <script src="resources/scripts/axure/adaptive.js"></script>
+    <script src="resources/scripts/axure/tree.js"></script>
+    <script src="resources/scripts/axure/init.temp.js"></script>
+    <script src="resources/scripts/axure/legacy.js"></script>
+    <script src="resources/scripts/axure/viewer.js"></script>
+    <script src="resources/scripts/axure/math.js"></script>
+    <script src="resources/scripts/axure/jquery.nicescroll.min.js"></script>
+    <script src="data/document.js"></script>
+    <script src="files/sign_up_-_school_created/data.js"></script>
+    <script type="text/javascript">
+      $axure.utils.getTransparentGifPath = function() { return 'resources/images/transparent.gif'; };
+      $axure.utils.getOtherPath = function() { return 'resources/Other.html'; };
+      $axure.utils.getReloadPath = function() { return 'resources/reload.html'; };
+    </script>
+  </head>
+  <body>
+    <div id="base" class="">
+
+      <!-- Unnamed (Header) -->
+
+      <!-- Unnamed (Rectangle) -->
+      <div id="u75" class="ax_default heading_1">
+        <div id="u75_div" class=""></div>
+        <div id="u75_text" class="text ">
+          <p><span><a href="home.php">REDU</a></span></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Rectangle) -->
+      <div id="u76" class="ax_default link_button">
+        <div id="u76_div" class=""></div>
+        <div id="u76_text" class="text ">
+          <p><span><a href="sign_up_-_selection_between_refugee_and_student.php">SIGN UP</a></span></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Rectangle) -->
+      <div id="u77" class="ax_default link_button">
+        <div id="u77_div" class=""></div>
+        <div id="u77_text" class="text ">
+          <p><span><a href="about_us.php">ABOUT</a></span></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Rectangle) -->
+      <div id="u78" class="ax_default link_button">
+        <div id="u78_div" class=""></div>
+        <div id="u78_text" class="text ">
+          <p><span><a href="contact_us.php">CONTACT US</a></span></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Rectangle) -->
+      <div id="u79" class="ax_default link_button">
+        <div id="u79_div" class=""></div>
+        <div id="u79_text" class="text ">
+          <p><span><a href="sign_in.php">LOG IN</a></span></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Shape) -->
+      <div id="u80" class="ax_default icon">
+        <img id="u80_img" class="img " src="images/home/u27.svg"/>
+        <div id="u80_text" class="text " style="display:none; visibility: hidden">
+          <p></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Rectangle) -->
+      <div id="u81" class="ax_default link_button">
+        <div id="u81_div" class=""></div>
+        <div id="u81_text" class="text ">
+          <p><span>CONNECT</span></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Image) -->
+      <div id="u82" class="ax_default image">
+      <a href="home.php">
+        <img id="u82_img" class="img " src="images/home/u29.png"/>
+      </a>
+        <div id="u82_text" class="text " style="display:none; visibility: hidden">
+          <p></p>
+        </div>
+      </div>
+      <div id="u74" style="display:none; visibility:hidden;"></div>
+
+      <!-- Unnamed (Rectangle) -->
+      <div id="u83" class="ax_default">
+        <div id="u83_div" class=""></div>
+        <div id="u83_text" class="text " style="display:none; visibility: hidden">
+          <p></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Shape) -->
+      <div id="u84" class="ax_default icon">
+        <img id="u84_img" class="img " src="images/sign_up_-_student_created/u40.svg"/>
+        <div id="u84_text" class="text " style="display:none; visibility: hidden">
+          <p></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Shape) -->
+      <div id="u85" class="ax_default icon">
+        <img id="u85_img" class="img " src="images/sign_up_-_student_created/u41.svg"/>
+        <div id="u85_text" class="text " style="display:none; visibility: hidden">
+          <p></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Shape) -->
+      <div id="u86" class="ax_default icon">
+        <img id="u86_img" class="img " src="images/sign_up_-_student_created/u42.svg"/>
+        <div id="u86_text" class="text " style="display:none; visibility: hidden">
+          <p></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Shape) -->
+      <div id="u87" class="ax_default icon">
+        <img id="u87_img" class="img " src="images/sign_up_-_student_created/u43.svg"/>
+        <div id="u87_text" class="text " style="display:none; visibility: hidden">
+          <p></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Rectangle) -->
+      <div id="u88" class="ax_default heading_3">
+        <div id="u88_div" class=""></div>
+        <div id="u88_text" class="text ">
+          <p><span>© [Company Name] 2022, All Rights Reserved</span></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Rectangle) -->
+      <div id="u89" class="ax_default heading_3">
+        <div id="u89_div" class=""></div>
+        <div id="u89_text" class="text ">
+          <p><span>Terms Of Service</span></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Rectangle) -->
+      <div id="u90" class="ax_default heading_3">
+        <div id="u90_div" class=""></div>
+        <div id="u90_text" class="text ">
+          <p><span>Privacy Policy</span></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Rectangle) -->
+      <div id="u91" class="ax_default heading_3">
+        <div id="u91_div" class=""></div>
+        <div id="u91_text" class="text ">
+          <p><span>Volunteer</span></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Rectangle) -->
+      <div id="u92" class="ax_default box_1">
+        <div id="u92_div" class=""></div>
+        <div id="u92_text" class="text " style="display:none; visibility: hidden">
+          <p></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Rectangle) -->
+      <div id="u93" class="ax_default box_1">
+        <div id="u93_div" class=""></div>
+        <div id="u93_text" class="text " style="display:none; visibility: hidden">
+          <p></p>
+        </div>
+      </div>
+
+      <!-- Unnamed (Rectangle) -->
+      <div id="u94" class="ax_default heading_1">
+        <div id="u94_div" class=""></div>
+        <div id="u94_text" class="text ">
+          <p><span>School Admin Account Created</span></p>
+        </div>
+      </div>
+
+      <div class="mx-auto" style="width: 200px;">
+        <a href="sign_in.php" id="SignIn" type="button" class="btn btn-secondary" >Sign In</a>
+      </div>
+    </div>
+    <script src="resources/scripts/axure/ios.js"></script>
+  </body>
+</html>
